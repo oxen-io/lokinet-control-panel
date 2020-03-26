9dd5fc6d8a6824e15d3e9f0850b059e0b2965752
Jason Rhinelander <jason@imaginary.ca>
2020-03-26 19:41:58 -0300

    debian updates for 0.2.1

--- debian/make-icons.sh
+++ debian/make-icons.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 set -e
+set -x
 
-for x in 128 96 64 48 32 24 16; do
+for x in 256 192 128 96 64 48 32 24 16; do
     mkdir -p build-debian/icons/${x}x${x}
-    convert -verbose -filter Catrom -resize ${x}x${x} res/images/icon.png build-debian/icons/${x}x${x}/lokinet-gui.png
+    convert -verbose -filter Catrom -resize ${x}x${x} res/images/icon.svg build-debian/icons/${x}x${x}/lokinet-gui.png
 done
+
+mkdir build-debian/icons/scalable
+cp res/images/icon.svg build-debian/icons/scalable/lokinet-gui.svg
