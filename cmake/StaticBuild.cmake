ad52eecee79a12d94d4335ecdc0c7310062bb117
Linux User <despair@alpine32.localdomain>
2020-10-05 01:52:51 -0500

    keep %ebp live

--- cmake/StaticBuild.cmake
+++ cmake/StaticBuild.cmake
@@ -123,7 +123,7 @@ build_external(zmq
   CONFIGURE_COMMAND ./configure ${cross_host} --prefix=${DEPS_DESTDIR} --enable-static --disable-shared
     --disable-curve-keygen --enable-curve --disable-drafts --disable-libunwind --with-libsodium
     --without-pgm --without-norm --without-vmci --without-docs --with-pic --disable-Werror
-    "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=-O2 ${flto}" "CXXFLAGS=-O2 ${flto}"
+    "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=-O2 -fno-omit-frame-pointer ${flto}" "CXXFLAGS=-O2 -fno-omit-frame-pointer ${flto}"
     "sodium_CFLAGS=-I${DEPS_DESTDIR}/include" "sodium_LIBS=-L${DEPS_DESTDIR}/lib -lsodium"
 )
 add_static_target(libzmq zmq_external libzmq.a)
