85011ff811d823bed095a6fc8926d8e932504035
Jason Rhinelander <jason@imaginary.ca>
2020-11-11 16:59:54 -0400

    Split up impossibly long line

--- contrib/build-win32-deps.sh
+++ contrib/build-win32-deps.sh
@@ -24,7 +24,12 @@ cd $SRCDIR
 mkdir qt5-build
 cd qt5; perl init-repository --module-subset=default,-qtwebengine,-qtactiveqt,-qtlocation
 cd $SRCDIR/qt5-build
-$SRCDIR/qt5/configure -prefix $SRCDIR/qt5-win32 -qt-zlib -qt-libjpeg -qt-libpng -qt-freetype -qt-pcre -qt-harfbuzz -opensource -confirm-license -platform $QT_HOST_PLATFORM -xplatform win32-g++ -release -device-option 'CROSS_COMPILE=ccache i686-w64-mingw32-' -opengl desktop -no-angle -skip qtwebengine -optimize-size -strip -ssl -openssl-linked -qt-sqlite -sql-sqlite -qt-assimp -qt-tiff -qt-webp OPENSSL_PREFIX=$SRCDIR/ssleay-win32 'OPENSSL_LIBS_DEBUG=-lssl -lcrypto' 'OPENSSL_LIBS_RELEASE=-lssl -lcrypto' 'OPENSSL_LIBS=-lws2_32 -lgdi32 -ladvapi32 -lcrypt32 -luser32' OPENSSL_INCDIR=$SRCDIR/ssleay-win32/include OPENSSL_LIBDIR=$SRCDIR/ssleay-win32/lib -qt3d-simd sse2 -ccache -skip qtactiveqt -skip qtlocation -nomake examples -nomake tests
+$SRCDIR/qt5/configure -prefix $SRCDIR/qt5-win32 -qt-zlib -qt-libjpeg -qt-libpng -qt-freetype -qt-pcre -qt-harfbuzz -opensource -confirm-license \
+    -platform $QT_HOST_PLATFORM -xplatform win32-g++ -release -device-option 'CROSS_COMPILE=ccache i686-w64-mingw32-' -opengl desktop -no-angle \
+    -skip qtwebengine -optimize-size -strip -ssl -openssl-linked -qt-sqlite -sql-sqlite -qt-assimp -qt-tiff -qt-webp \
+    OPENSSL_PREFIX=$SRCDIR/ssleay-win32 'OPENSSL_LIBS_DEBUG=-lssl -lcrypto' 'OPENSSL_LIBS_RELEASE=-lssl -lcrypto' \
+    'OPENSSL_LIBS=-lws2_32 -lgdi32 -ladvapi32 -lcrypt32 -luser32' OPENSSL_INCDIR=$SRCDIR/ssleay-win32/include OPENSSL_LIBDIR=$SRCDIR/ssleay-win32/lib \
+    -qt3d-simd sse2 -ccache -skip qtactiveqt -skip qtlocation -nomake examples -nomake tests
 make -j$JOBS; make install
 #cd $SRCDIR/libsodium
 #CC='ccache i686-w64-mingw32-gcc' ./configure --host=i686-w64-mingw32 --target=i686-w64-mingw32 --disable-shared --prefix=$SRCDIR/sodium-win32
